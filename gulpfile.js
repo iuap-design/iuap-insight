4948b3684f0e12e5db77beb893481502251c617a
guoyff <guoyff@yonyou.com>
2016-11-02 09:35:44 +0800

    update(lib)

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,11 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-// 压缩 js 文件
-// 在命令行使用 gulp script 启动此任务
 gulp.task('script', function() {
-    // 1. 找到文件
-    gulp.src('src/*.js')
-    // 2. 压缩文件
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-    // 3. 另存压缩后的文件
-        .pipe(gulp.dest('dist'))
-
+  return gulp.src('src/*.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('lib'))
 })
+
+gulp.task('default', ['script'])
