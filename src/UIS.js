8b77780db93cea5971e1567e5af1aa56171aac2a
“duxue7 <duxue7@yonyou.com>
2020-11-24 12:39:16 +0800

    发送请求之后清楚TraceId

--- src/UIS.js
+++ src/UIS.js
@@ -444,6 +444,7 @@ UIS.fn.report = function (data, block, callback) {
         device: this.getDevice(),
         ...data
     }
+    this.setOption("traceId","")
     
     // Utils.post(this.getOption("trackerUrl"), reportData)
     if (navigator.sendBeacon) {
