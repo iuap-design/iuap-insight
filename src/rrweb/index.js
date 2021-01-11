d8b081df54b8288e1145fdd96612b7826dddc68a
“duxue7 <duxue7@yonyou.com>
2021-01-11 11:27:13 +0800

    fix：还原rrweb

--- src/rrweb/index.js
+++ src/rrweb/index.js
@@ -1,6 +1,6 @@
 
-// import * as rrwebSDK from 'rrweb';
-import * as rrwebSDK from 'hubble-record';
+import * as rrwebSDK from 'rrweb';
+// import * as rrwebSDK from 'hubble-record';
 
 /**  -------  记录上传的逻辑 开始   ------ */
 
