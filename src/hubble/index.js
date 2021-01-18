cb579ea694d77a783c8c34fa220e320acf6858ef
“duxue7 <duxue7@yonyou.com>
2021-01-18 14:36:55 +0800

    fix：cooike添加path

--- src/hubble/index.js
+++ src/hubble/index.js
@@ -211,7 +211,7 @@ class Hubble {
     var Days = 30;
     var exp = new Date();
     exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 30);
-    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString() + ";domain=" + domain +";SameSite=None;Secure";
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString() + ";path=/" + ";domain=" + domain +";SameSite=None;Secure";
   }
 
   /**
